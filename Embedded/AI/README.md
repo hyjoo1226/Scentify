2a8102f5bbeffac5daf24a1109c2bd0368ad51e3
seof622 <seof622@naver.com>
2025-01-17 15:07:59 +0900

    feat: re add add yolov8 pose estimation sample code
    Former-commit-id: feb6e9aae4609181eae920ae4c03d308c7f21acd

---
+++ Embedded/AI/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:114febb5e92399dfc3187df75f4c2d568ce836f9140dddfccc80c93e02f2a63a
+size 715
---
+++ Embedded/AI/yolo_test.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2c421bf49f61143729766d2ab12d25c4d76f63e549dcb5ece2cf4a716f8b574c
+size 867

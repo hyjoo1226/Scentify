f3cd1f7a10665b0dfb36eb40664b438c9e06681a
SunJung <djtjswjd1@naver.com>
2025-01-21 15:08:29 +0900

    [FEAT] S12P11A205-143 인증코드 검증 구현
    Former-commit-id: cdbe41c35a70c4a7479fc88211ab17f702eee024

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/controller/UserController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19970a13f5024e81ade92e8fac2a209590ddcdc890493720f4625c8c2acfb21e
-size 3576
+oid sha256:c6e2e10e29057999f3bcd6e6979eba727eec6fbc344aa0d1f315fecf21fa24b8
+size 4844

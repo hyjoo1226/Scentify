3db0b4656fb677c1bfe3d41d75eeb04952f06729
seof622 <seof622@naver.com>
2025-01-22 12:29:49 +0900

    fix: S12P11A205-139 solve mqtt, websocket async problem
    Former-commit-id: 7dae6532e94cec2a05d15d313baf648c55ed3bd4

--- Embedded/Websocket/src/websocket_client.py
+++ Embedded/Websocket/src/websocket_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12d880d7ffcba3b3617ba6a4d8ef77577bf32a081184234b37a3106cf1c5e98b
-size 805
+oid sha256:4068bb567973aab65273c8dfafd5cb267fc8de0d6bab358d042dc11f2d1c8ce9
+size 838
--- Embedded/Websocket/src/websocket_server.py
+++ Embedded/Websocket/src/websocket_server.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23b4ec4f490e881965fd39eb9b53642f53c0c88211cf66e6d9a8772e13f8c7fb
-size 2275
+oid sha256:a4df605734f1822e9bcd4a59443fc19650df7cc77d9ee52d30c05f4ee40d8961
+size 2064

96004b90c871a7f801de50a3c036c7aff3a8f3d5
seof622 <seof622@naver.com>
2025-01-22 18:23:56 +0900

    feat: S12P11A205-139 complete getting raspberry pi serial number test
    Former-commit-id: 8190b847c1a4e2f3a06bf458da205fa83cd16937

--- Embedded/Websocket/src/websocket_client.py
+++ Embedded/Websocket/src/websocket_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4068bb567973aab65273c8dfafd5cb267fc8de0d6bab358d042dc11f2d1c8ce9
-size 838
+oid sha256:1af710b3ad5ea3631f446e6636326ff7ea780f296ff07025467e3c2c98fdbe2c
+size 1412

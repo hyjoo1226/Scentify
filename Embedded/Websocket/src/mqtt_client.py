8e1cfe1754cfa7dd037af38ca3de5324b1c398c8
주현호 <sparrow9576@naver.com>
2025-01-22 14:17:51 +0900

    feat: 웹소켓 MQTT 연결
    Former-commit-id: d585ee71d0df33399ceab1cb49c70bb4c8b1387a

---
+++ Embedded/Websocket/src/mqtt_client.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f0c9007c00bf5b3e49a25af05190772a167fb6d15f832ca484ff324c160fef17
+size 965
--- Embedded/Websocket/src/websocket_server.py
+++ Embedded/Websocket/src/websocket_server.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fec12911c3c2cf8ee49ca82f8e657ef10782441ba037a6625e014dff96be2696
-size 1958
+oid sha256:23b4ec4f490e881965fd39eb9b53642f53c0c88211cf66e6d9a8772e13f8c7fb
+size 2275

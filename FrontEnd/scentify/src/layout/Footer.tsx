20b49c5a3a7132d01da456e92ab7dbe349f0eca8
leegayoung <leegy0720@naver.com>
2025-01-23 12:36:09 +0900

    💄 style: [user] scent 아이콘 수정 <이가영>
    Former-commit-id: 790d2f64e090f965642d80b3614c22d3f215d7f2

--- FrontEnd/scentify/src/layout/Footer.tsx
+++ FrontEnd/scentify/src/layout/Footer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4d1c1e86d345f3af59d70b629a09ebb5f8616df171273313ba7687f8514588d
-size 1131
+oid sha256:8ab97a114d384b65360c9e2d86718d8d9319642a6e8acce3a81fc5dacc9cd506
+size 1132

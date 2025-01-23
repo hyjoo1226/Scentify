a360b3e1fe0ac436349b8a3513e7b26272c295c2
이가희 <lgh7956@naver.com>
2025-01-23 13:50:14 +0900

    ✨feat :[Home] 기기 관리/추가 UI컴포넌트 완료 <이가희>
    Former-commit-id: 1453134310a829fbe323bc8bcb0d004baa2c7f3a

---
+++ FrontEnd/scentify/src/feature/Home/Device/DeviceCard.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b922dde1e91256d01453cb303e6d6c60f1604dd58a9e7a3e8c38e3911b495866
+size 3514
--- FrontEnd/scentify/src/pages/Home/Home.tsx
+++ FrontEnd/scentify/src/pages/Home/Home.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60881bf042afcbf8bf81594920741c3627683387c16b636444886e4e1f1a7b22
-size 767
+oid sha256:3d4f56352bea7dea3d3b7e748a833ce76de0b0eb01c4387af7c7524311f77344
+size 878

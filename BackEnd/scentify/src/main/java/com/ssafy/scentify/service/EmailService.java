9a40d3adeafe92f9499ac0d9059af272b3fcee01
SunJung <djtjswjd1@naver.com>
2025-01-24 22:11:03 +0900

    [FIX] 네이버 SMTP로 변경
    Former-commit-id: 9038363d4a3a1d2a3750856c6dc04bb772af9a16

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/service/EmailService.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/service/EmailService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:465f0fbde043e3516768bd73f936a61f22fbfd7853ff454e95ce2c4c8d36be72
-size 1609
+oid sha256:8c9f89fd25cc894a49bf1067da8946692ceb1b5027fb03c30b6c3fc3e6f32d42
+size 1611

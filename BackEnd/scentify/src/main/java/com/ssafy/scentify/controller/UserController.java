25106b24e20d78053d5f1dc98bee875c32844eef
SunJung <djtjswjd1@naver.com>
2025-01-25 16:48:57 +0900

    [CHORE] 유저 컨트롤러 주석 추가
    Former-commit-id: de552b4720fc368eb86078e6f98408815a7f99b6

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/controller/UserController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:761615d5e1f7896d19e96b9be015eb039e769c70efa3ddbf3fbc897955b53b34
-size 9648
+oid sha256:fc30313c26e9ea21d3f0887cf8c58fcc95a946be8bde243102c1e21e60d494a4
+size 10496

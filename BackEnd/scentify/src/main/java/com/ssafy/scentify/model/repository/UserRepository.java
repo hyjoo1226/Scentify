09a04b433fd1e9d95f9176b8ba62ce338c0abf65
SunJung <djtjswjd1@naver.com>
2025-01-25 18:56:25 +0900

    [FEAT] S12P11A205-115 유저 사진 번호 수정
    Former-commit-id: 0818cae498bfda3b9699376b2b5dd815273f111e

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/model/repository/UserRepository.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/model/repository/UserRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56c16ace36b1626b2544b8284b5687c04644b6b9d3f04341054332e1eea8a893
-size 1741
+oid sha256:6f6acd46763b1ddee9302bd8a9f2e3f34e5b7e97afbc40285a447cba2a45b93a
+size 1921

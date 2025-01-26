32e9112bb43010f1290f0e9986f9ea4201b495ef
SunJung <djtjswjd1@naver.com>
2025-01-26 11:08:59 +0900

    [FEAT] S12P11A205-116 비밀번호 재설정 구현
    Former-commit-id: 1ee91f3c0152dd59c264ac7e1cc35d7b34e5dd9f

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/model/repository/UserRepository.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/model/repository/UserRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f6acd46763b1ddee9302bd8a9f2e3f34e5b7e97afbc40285a447cba2a45b93a
-size 1921
+oid sha256:23a99cf71cd556ea975e8f0e13bad2b08d014fc6f8f18427ee6cb2f9c41b1df0
+size 2103

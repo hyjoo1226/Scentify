1b7d0ecc64e9eeed991c08b048fc5653d0b70ab5
SunJung <djtjswjd1@naver.com>
2025-01-26 12:48:49 +0900

    [FIX] webConfig 수정
    Former-commit-id: bec0482238055746eb878e4a09fbce3875598f4b

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/common/config/WebConfig.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/common/config/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b8e00e516278c26952d2e55fb3e9197e1389db993321fc6ab62ecae4fac341e
-size 542
+oid sha256:c42cb6083576a9af12bedce99aeb29cf804cc18f051cdae596ae93385c0ed66b
+size 648

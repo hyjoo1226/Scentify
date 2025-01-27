fa56c5a541cb9b74675eeb6e71a8395ae61becd1
SunJung <djtjswjd1@naver.com>
2025-01-27 17:55:41 +0900

    [FEAT] S12P11A205-157 S12P11A205-158 RB 캡슐 정보 송신 + 캡슐 잔여량 수신 구현
    Former-commit-id: 30a562555c0df58c11bec1a7ad67a01cd220aeca

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/common/util/TokenProvider.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/common/util/TokenProvider.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8184d3a1aedaa6f0841ff83ac338459c021b956fd01b135380754b311a82ab2d
-size 4360
+oid sha256:039b866b453ffd7eb053514cc9022c539d57a86a28c070970b25db4c35b450f3
+size 4369

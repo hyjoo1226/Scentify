19fdbe41bc8857ce39228f6df1151fd46e9f1ca4
SunJung <djtjswjd1@naver.com>
2025-01-27 16:19:28 +0900

    [FEAT] S12P11A205-171 기기 정보 조회 기능 구현
    Former-commit-id: 8c6b6ef7d80a9795b35fcf39a2483db9d7116cc2

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/device/model/repository/DeviceRepository.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/device/model/repository/DeviceRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:691f29d40139b4f20face039bf045b0c0e9c1029098de348d4a29e47596e3819
-size 1255
+oid sha256:39c3808c7434c9c51d71e030f9dd49ac2233aef6d49c6632c6ec69b3460fbea2
+size 1469
--- BackEnd/scentify/src/main/java/com/ssafy/scentify/user/controller/UserController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b32397ae39d8fc483a3912f8dca83a2d97329b87899bc0ee73ffac71a00989d7
-size 20503
+oid sha256:9309c997cdfc41ee6d9c30e88b3c7df8835be9a055dd2b7eec620aa1ab47a45b
+size 20501

d4d9eb422016863fc35b62ad2dc0338aa0df0826
SunJung <djtjswjd1@naver.com>
2025-01-27 16:21:25 +0900

    [CHORE] 주석 추가
    Former-commit-id: 2caa39782d3a2752aeaf7ddeab785cf3d43bd08e

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11dc60a7698dd9e5687d61331fbe74a1cb4648d8fd3ea683549b1b9d9774acfc
-size 9517
+oid sha256:3024d79a487a99cbd48315357dc5fbd32e62178c87023b6e86f76a9051f51be4
+size 9554

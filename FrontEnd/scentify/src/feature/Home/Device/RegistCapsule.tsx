40e6dc43836e42588c46a7d11a86fcfcc34759bd
이가희 <lgh7956@naver.com>
2025-01-27 12:25:13 +0900

    ✨[Home] 홈메인 캡슐등록 컴포넌트 UI 재구현, env파일 생성 <이가희> S12P11A205-185
    Former-commit-id: a2ce1a62a0558b02af36ad093e8883fd7937b06c

--- FrontEnd/scentify/src/feature/Home/Device/RegistCapsule.tsx
+++ FrontEnd/scentify/src/feature/Home/Device/RegistCapsule.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:090cd63dadb59a97b0fa97455a5925b573e32a381f5e346bcce51ee5083bc310
-size 3568
+oid sha256:020e5272446d6609dc7a98d203b033701b250f85093c48099f45f8aedd5f8279
+size 4565

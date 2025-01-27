75b2c6e9192733f8242fab5cfdb5b712b565e6df
SunJung <djtjswjd1@naver.com>
2025-01-27 15:30:28 +0900

    [FEAT] S12P11A205-170 대표 기기 설정 기능 구현
    Former-commit-id: 3ec62db30fc539bd24c5ad0b641d99f83b145ce9

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/user/controller/UserController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8a7bb91b43f8ac500fc935aee62441006e6f933f78e92a15a7e2422c1c2193c
-size 19506
+oid sha256:b32397ae39d8fc483a3912f8dca83a2d97329b87899bc0ee73ffac71a00989d7
+size 20503

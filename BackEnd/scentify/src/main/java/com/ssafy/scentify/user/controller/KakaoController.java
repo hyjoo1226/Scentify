d3f68214b1e4e0ed03618a494065a3013c1f87e4
SunJung <djtjswjd1@naver.com>
2025-01-28 18:41:16 +0900

    [FEAT] 카카오 로그인/ 회원가입인 경우 초대링크로 들어왔을 떄 로직 추가
    Former-commit-id: 85338c335a251f9997a1b36248affe3a1bc8d0ac

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/user/controller/KakaoController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/user/controller/KakaoController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb3ab56acee2faead46c2b8dfffd48e402bf4c7df7bdf481ef243ef2a0078c8e
-size 6563
+oid sha256:101b2b86fdeacc66e71da010ef766bc07384ee957b5d07349f6091fa519f79bc
+size 9604

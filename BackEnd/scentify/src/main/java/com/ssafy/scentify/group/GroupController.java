936753d6150eeb43a1d0630e2acca8c8cf8a72eb
SunJung <djtjswjd1@naver.com>
2025-01-28 16:49:01 +0900

    [FEAT] S12P11A205-165 초대 링크로 그룹 가입 컨트롤러 구현
    Former-commit-id: 78b8dda6260a38bcefa53a9e0d02bea475147629

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/group/GroupController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/group/GroupController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f970df02e9f6c9f0f298eeb5f2db5ae4e8d6f9c9ca07968c77588beb7119aa1
-size 11679
+oid sha256:2e389af6a9ea676ea15265d9f34e2c15eeceef7f26bab9f25edf4bc0704ac8c8
+size 13443

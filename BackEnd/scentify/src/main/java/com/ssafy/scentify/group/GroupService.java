510c54978b6061c9f8b4fa08da73ad1a8363c27e
SunJung <djtjswjd1@naver.com>
2025-01-28 15:28:27 +0900

    [FEAT] S12P11A205-166 초대 코드로 그룹 가입 구현
    Former-commit-id: 872a036f635547511ddedfa046c887652279a46b

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/group/GroupService.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/group/GroupService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f2a29909d9273e927810dfeb30530d6d8f7170a1efe7b9156fbc3aa6bf212e1
-size 644
+oid sha256:23b66e62b3133358a115033b2fe13a248d84b97b3ec4b0f130237b8d11199d6b
+size 812

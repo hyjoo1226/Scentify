8238de017ef4cd858ed53a987b7185d3b35f8b39
seof622 <seof622@naver.com>
2025-01-30 01:39:07 +0900

    refactor: change nothing action enum name
    Former-commit-id: f90cc263c054b7e76308c8357fc9c089c95f0cb2

--- Embedded/AI/modules/slowfast.py
+++ Embedded/AI/modules/slowfast.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b39560cb58d1e54453e3a30dd6c549356c2d8e42d16fec3147ed4f1afce4330
-size 3575
+oid sha256:3b2bd4db661d9abaac38ce7920e5a01d2f7fe5379096db072ccc67f81ad2fbfe
+size 3569

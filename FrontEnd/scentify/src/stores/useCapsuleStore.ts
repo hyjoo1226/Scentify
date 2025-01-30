bacc169e8f3bfa19dd71c89921b8e3b958d78178
이가희 <lgh7956@naver.com>
2025-01-30 10:21:12 +0900

    :sparkles: [Home] 초기 캡슐 등록, 기본향 등록 전역상태관관리 구축, 전반적인UI, 파일정리 <이가희> S12P11A205-221
    Former-commit-id: 5f8f8daf876d000d1c876194c1700fbf802be8d9

--- FrontEnd/scentify/src/stores/useCapsuleStore.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:03f47445d077f3d4dfe1745cb805fc04c6f532178cf294a04a4777258a8d7142
-size 1387
--- FrontEnd/scentify/src/stores/useDefaultScentStore.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e4881d0a04374e9858591160952d7650d49ef485021cb975daf58a3608d36f01
-size 941

a36fdff17879e2ac3d762dc942bb2e30d17917b7
어선정 <djtjswjd1@naver.com>
2025-01-31 10:10:15 +0900

    [CHORE] 리다이렉트 주소 일부 수정
    Former-commit-id: 655b7d79a0dc4cad2a9808ccf7f8a8874b2f6137

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/user/controller/KakaoController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/user/controller/KakaoController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff8484b0e4d1ac7b89576e12b3df7526aa445c8e02e64f9e26c71e01a87fb476
-size 11106
+oid sha256:de043fcc37bbb9e2de9eb0c2be898ce0e0af58662294f383451c5f037d75dc73
+size 11171

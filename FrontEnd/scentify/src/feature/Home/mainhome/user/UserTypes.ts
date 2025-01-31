adca8644ffca7c3a205ea0ec88d0b68fd7aec59b
leegayoung <leegy0720@naver.com>
2025-01-31 14:16:41 +0900

    ✨ feat: [My] 마이탭 기본틀 형성 <이가영>
    Former-commit-id: 9c0f5572bf15eeba65e8e0201a00c4041d6f70da

--- FrontEnd/scentify/src/feature/Home/mainhome/user/UserTypes.ts
+++ FrontEnd/scentify/src/feature/Home/mainhome/user/UserTypes.ts
@@ -1,7 +1,9 @@
 // 사용자 데이터 타입 정의
 export interface UserData {
+  userId: string;
   userName: string;
   imgNum: number; // 프로필 사진 번호
+  mainDeviceId: number;
   date: string;
   weatherIcon: string;
   weatherDescription: string;
---
+++ FrontEnd/scentify/src/feature/my/components/MyUserCard.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4682411575f58d854cf859c22cd6020481895dd6945355a2356cd6dfb0eef605
+size 4870

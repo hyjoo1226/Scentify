cb35e5db52ada4c5afc414980e6d87e077dd05fd
이가희 <lgh7956@naver.com>
2025-01-31 13:20:42 +0900

    ✨ feat: [Scent] 향 캐러셀, 향조합 찜리스트 컴포넌트, UI 완성 <이가희> S12P11A205-227
    Former-commit-id: 12fa82e66d01bf1b82e91ef40be7b14450906f66

--- FrontEnd/scentify/src/feature/scent/scentmain/ScentMain.tsx
+++ FrontEnd/scentify/src/feature/scent/scentmain/ScentMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a41cc513763f249c9c02dcb42c4668a675e7288ae2af830f5c758de2aa0c85c
-size 2975
+oid sha256:38039ab877b256a659010eb56a0422a6d7ed31b97906d0a5be3c66f147a3d5ce
+size 2972

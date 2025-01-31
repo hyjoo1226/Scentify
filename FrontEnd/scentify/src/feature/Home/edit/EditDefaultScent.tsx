598a38a0ce5f79dc0d21e6d4183d64a53f547ddf
이가희 <lgh7956@naver.com>
2025-01-31 14:12:11 +0900

    :bug:fix: Layout 수정
    Former-commit-id: 34dd3d86bca5f33cfa3252cce866478ad120a232

--- FrontEnd/scentify/src/feature/Home/edit/EditDefaultScent.tsx
+++ FrontEnd/scentify/src/feature/Home/edit/EditDefaultScent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9867fa14b79a3aad4830cbe9226c81f5fee79c47931b3a50d905f05d7dafd791
-size 3036
+oid sha256:772b6008c2ba01bb48c9ee9539a24fe02165efa004b0ac554f67d72080528237
+size 3143

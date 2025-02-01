b5a70d2f2150459912e3ea94bfa6eadaa7eee013
SunJung <djtjswjd1@naver.com>
2025-02-01 22:49:19 +0900

    [CHORE] 전달 파라미터로 기본향id와 room type 추가
    Former-commit-id: c83758a31895f820a225381c5ebdd54c62cf81af

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/device/model/dto/DeviceDto.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/device/model/dto/DeviceDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6f2fb15c3aa27f9d129914c8addc79f8cec478f2ce6e1dea715696c4a91f09a
-size 4818
+oid sha256:b8f91d301f608f5cc8fc67de83e83db4305f145fdd0b491f7fa8b4ab18a190c3
+size 4883

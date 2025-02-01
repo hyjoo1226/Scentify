4bc75719937c1e49d32215f150296edf95c15e5e
SunJung <djtjswjd1@naver.com>
2025-02-01 15:01:47 +0900

    [CHORE] 전달 파라미터 id로 통일
    Former-commit-id: 0804bbef2035ec4f34eda5d907ac246dbbcdc970

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/device/model/dto/DeviceDto.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/device/model/dto/DeviceDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33c17c10054b52bfaac5bd9a7fb9aa25d83aebb0b83336b497b0ed4b11f9f747
-size 4824
+oid sha256:e6f2fb15c3aa27f9d129914c8addc79f8cec478f2ce6e1dea715696c4a91f09a
+size 4818

928ef8f4cee2ad527a9671d608554f444fd5ed02
ath <seof622@naver.com>
2025-02-03 01:23:19 +0900

    feat: add websocket receive message handler
    Former-commit-id: 2ad47cd3ee0fdeb3e0922e917784bb9882ca6437

--- Embedded/Websocket/modules/client/websocket_client.py
+++ Embedded/Websocket/modules/client/websocket_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d3242abc29248485e06d6fac927eab39524a45f3713df6ae8a67b5bff982069
-size 3370
+oid sha256:c78ccb7a4f228c3c3f406ad1fbc0a1a8473c4a2a7ba47f020edd25858cae7ccb
+size 3945

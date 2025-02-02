907c26fd11464d699a4e7014c81b8291aa6bb21d
ath <seof622@naver.com>
2025-02-03 00:52:18 +0900

    feat: divide full logic into init, recevie_message, send_message
    Former-commit-id: 32e3eeff812d215ff1b1fd34e6e46c92b524741f

--- Embedded/Websocket/modules/client/websocket_client.py
+++ Embedded/Websocket/modules/client/websocket_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af6ffebaa12a27f32174858d057fb581376970828dde11bc330fc5030fb93072
-size 2627
+oid sha256:5d3242abc29248485e06d6fac927eab39524a45f3713df6ae8a67b5bff982069
+size 3370

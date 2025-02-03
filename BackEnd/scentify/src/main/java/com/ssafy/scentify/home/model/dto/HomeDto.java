4109f3804d32337cbf14f55244fdc5c54a81deb2
SunJung <djtjswjd1@naver.com>
2025-02-03 17:01:09 +0900

    [CHORE] nickname으로 통일
    Former-commit-id: 8e0daa4310e0ac3e5f3b51de2ed44eb402a44320

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/home/model/dto/HomeDto.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/home/model/dto/HomeDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:963fa67aa9af05a3ce42e8c2a629199839d12c93986de91aa89b83f7af62bd88
+oid sha256:1598e4ce064f07e90bff622df0aa033408edf1102b89322fb36483080f27f8e2
 size 1827
--- BackEnd/scentify/src/main/java/com/ssafy/scentify/user/controller/UserController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:658474eedf623f93a79ce3ce199b3755d0dc04ed2cc5ab93fbbc75029588dc81
+oid sha256:ce1da6cc33111f9747e68137f311e63641187d342c23a9bd23793d81f43341c0
 size 23085

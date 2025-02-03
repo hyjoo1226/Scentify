a2683d70260045248ade1c1174f1ba6c3492d227
주현호 <sparrow9576@naver.com>
2025-02-04 01:42:23 +0900

    🐛  fix: [Control] 중복, 주석 제거 <주현호>
    Former-commit-id: a474a040541b02f479db70d734fc16149a1d447e

--- FrontEnd/scentify/src/components/Control/HomeScentSetting.tsx
+++ FrontEnd/scentify/src/components/Control/HomeScentSetting.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9024d240f997f245d5ef16f9b6655aba929312d2678c8203067480df66d23194
-size 5927
+oid sha256:804e6d1a37ef955e2a25ff65ff0732f5e9cbf0abdb3d88cec225b9628b2dd527
+size 2827

a07d506afdb198a9b9ce5e49fc140b5bdd1cbe19
seof622 <seof622@naver.com>
2025-02-03 11:05:22 +0900

    feat: change websocket client to singletone
    Former-commit-id: 20cbab9a4bfbe0390cac669142d62b41433440ae

--- Embedded/Websocket/modules/client/websocket_client.py
+++ Embedded/Websocket/modules/client/websocket_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d37a6fc0971fd12a2142b02c942c9a6bf7a7e5ba7c725db03df27c1ae3b3eac5
-size 4084
+oid sha256:0d1765ee9cb8e037df94b43d5df40a709189594b3fa82e57e750754e0128e37c
+size 4644

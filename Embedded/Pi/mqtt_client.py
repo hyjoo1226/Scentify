d22ef37e8d44536bb9220354e2bbbac2271c13d4
seof622 <seof622@naver.com>
2025-02-03 16:37:29 +0900

    feat: S12P11A205-254 add receive capsule info logic
    Former-commit-id: 276b5b71073f2aaa3c0266fbd1b16d9f6148588b

--- Embedded/Pi/mqtt_client.py
+++ Embedded/Pi/mqtt_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:912f912e1b7f9f15f1bd2f8e5828651322c5f22a27f28eca3310dd28c310bb11
-size 3353
+oid sha256:731f3fc02355e1bd3a66bc511b930e7a6b89b48a5641054b01ee53eb45a112d8
+size 3731

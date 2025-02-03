5fa34110dc0ef473f1a0d875eb88618b9036d42b
어선정 <djtjswjd1@naver.com>
2025-02-03 21:16:16 +0900

    [DEBUG] Array import
    Former-commit-id: fa72d0c6948425b6a081a3c8dde7d589481f4f79

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b1cbf6ff5955fa701ed171fb1140265e41e5e547e6610cb1caa6de48cbfe0ba
-size 11505
+oid sha256:14a5613375e38f9dec9efbf5bfdc35399e36affe170ef99db814724951827ae3
+size 11530

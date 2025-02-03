4ae82531eb7691983a9fa52089b7a00323341d1d
seof622 <seof622@naver.com>
2025-02-03 17:13:38 +0900

    feat: S12P11A205-254 complete mqtt communication with pi
    Former-commit-id: f68941b39f0c1fd77fa827df70df42e4015763f2

---
+++ Embedded/Jetson/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6542a4241aa4a32c13d3218ba9919c069af10674e8264fd89b5dad8d51ea98cb
+size 2771
---
+++ Embedded/Jetson/mqtt_client.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f964af88348d132ba6b041ebd298b1e8223646e5f71add1078ea9ec407843730
+size 2193

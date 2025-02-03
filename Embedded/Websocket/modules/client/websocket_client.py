92bb01bee11926757de559f58387f0c23c575115
seof622 <seof622@naver.com>
2025-02-03 17:48:03 +0900

    refactor: change function name (test websocket connection -> connection)
    Former-commit-id: 1f4d86a1de9781babd5ea4db9a1c2ccf8828f707

--- Embedded/Websocket/modules/client/websocket_client.py
+++ Embedded/Websocket/modules/client/websocket_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05b7679f405b3e17a61cc5b3c3b1839108e8fcf53d269835d25480af73185494
-size 4820
+oid sha256:763094bb426f924cf08e48bef6699d21750039447ed4cf3e39cdefd67acd6d5a
+size 4821

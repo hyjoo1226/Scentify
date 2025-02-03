357f1f2b4404eba7bed89555b1fab959fa4df60f
이가희 <lgh7956@naver.com>
2025-02-03 13:57:54 +0900

    ✨ feat:[Home] 홈페이지 기기등록API연결 <이가희> S12P11A205-261
    Former-commit-id: aaaef7c3322f7c1fb939b7a9e4379792fad323c1

--- FrontEnd/scentify/src/feature/Home/deviceregistration/RegistDevice2.tsx
+++ FrontEnd/scentify/src/feature/Home/deviceregistration/RegistDevice2.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5dfa6d0a0b89cb8097e3d599609052dd505a389193b536ef5831bb097b5d81eb
-size 3013
+oid sha256:e23d280721d7378e9ac1a492d2ddbaa2c091c86c15600974b475a59c9b86fe4d
+size 3396

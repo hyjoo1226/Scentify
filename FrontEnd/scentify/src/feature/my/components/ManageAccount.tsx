ee610b2c81d07bb9145d7f243562c1728c4ac7b2
이가희 <lgh7956@naver.com>
2025-02-04 13:56:07 +0900

    ✨ feat: [My] 탈퇴하기, 모달 비밀번호 검증 API연동 <이가희> S12P11A205-270
    Former-commit-id: dc895096b2d6712e3a3219d9d3f1f604f1a20885

--- FrontEnd/scentify/src/feature/my/components/ManageAccount.tsx
+++ FrontEnd/scentify/src/feature/my/components/ManageAccount.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f4937eb7dcd683bd39e6e2af7110164ce313bcca9bce3d1b8c064f4e019f5dc
-size 5568
+oid sha256:d7231a1db1ff91de96489421cded9fc902c8d17429c531472793a183ef03652c
+size 6453
--- FrontEnd/scentify/src/pages/Home/HomeMain.tsx
+++ FrontEnd/scentify/src/pages/Home/HomeMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96376ab46dea8e648b0310eee5600fc02ac4349f1142fb9c69c016642acaef7a
-size 2864
+oid sha256:ac848223bba7142dffc5ef5426911adcd6c7f694edcd96df1948508b362684cb
+size 2886

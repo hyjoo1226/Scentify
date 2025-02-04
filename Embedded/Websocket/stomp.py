00741547dd875836c8c26c037c5e0bfc8d1acfaa
seof622 <seof622@naver.com>
2025-02-04 18:19:20 +0900

    refactor: clean up code
    Former-commit-id: e677219315499219fe18a27227efffece8e3d69f

--- Embedded/Websocket/stomp.py
+++ Embedded/Websocket/stomp.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee0c0174d84340836f70f4a72d635bb8d259d04ddaf781c18238fcd26aef4f21
-size 1493
+oid sha256:9a613fa59b44b378fdab31429418a293158eea619155080bcfa9052715462c5c
+size 1279

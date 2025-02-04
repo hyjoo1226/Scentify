5acc0e3353660d09b99528fb8504745828807652
leegayoung <leegy0720@naver.com>
2025-02-04 13:36:57 +0900

    🐛 fix: [home] 기기 더미 데이터 삭제 <이가영>
    Former-commit-id: 48efd1914affe8fc418b2382f3ff86629d875d9f

--- FrontEnd/scentify/src/pages/Home/HomeMain.tsx
+++ FrontEnd/scentify/src/pages/Home/HomeMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1cbda17cdbbd793bf6d27a0ab52adfa1fa5adc36ec571ca9c517103c9389460
-size 2963
+oid sha256:623022ec9b79e00c9829bcde65ff65254a34e19cbb9ce88680687c137ff0d988
+size 2392

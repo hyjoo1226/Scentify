e8fe288b76bf654fb98e70b4ad8c8eb7af257b72
seof622 <seof622@naver.com>
2025-02-04 18:28:42 +0900

    refactor: clean up code & make method
    Former-commit-id: 3cda21900b56c4789af6c18e1b5741448cc854bb

--- Embedded/Websocket/modules/client/websocket_response_handler.py
+++ Embedded/Websocket/modules/client/websocket_response_handler.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e45070117fee1772546244c823b40483c36da46c914777da643dee82c20f116b
-size 1364
+oid sha256:9b879a9e5dd60e0eb9bbc91370bdf079d8d7158917e09df6c47ebf8ad8c99810
+size 1545

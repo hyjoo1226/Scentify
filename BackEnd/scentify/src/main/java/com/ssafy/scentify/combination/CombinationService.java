e34a029fa36bb8f7afddaa80bfaf0dcc183e1dba
SunJung <djtjswjd1@naver.com>
2025-02-05 21:32:23 +0900

    [FEAT] S12P11A205-272 RB 전체 자동화 모드 정보 요청
    Former-commit-id: 3a67f2668c36d65521b8ce95ded5ee80293cdaea

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/combination/CombinationService.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/combination/CombinationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da985a679205b6998495030f27a0fc62af8f023ce86f2e04368bbe9bf5a06b65
-size 1290
+oid sha256:ecf68ba179266f95e8fe61962364e36c47daaf716ccc1d3f1085cb71634f31ba
+size 1531
--- BackEnd/scentify/src/main/java/com/ssafy/scentify/websocket/WebSocketController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/websocket/WebSocketController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46a91bc4a7a8cffe990023bc199d5cdf900eef510e875ff2a13a79e0c4a59c51
-size 10840
+oid sha256:04ba438370b253f98ee3a13735a706e5356300a3ff1b6a1e734eccd87529e90b
+size 11641

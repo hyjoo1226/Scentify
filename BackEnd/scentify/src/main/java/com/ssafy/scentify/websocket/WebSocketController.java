21f22949c3fab76abfde56c881f1c1eb382c5405
SunJung <djtjswjd1@naver.com>
2025-02-06 08:26:36 +0900

    [CHORE] 매핑 경로 수정
    Former-commit-id: cd51a692a230bc29a36e52cf697adaee2cb6c22b

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/websocket/WebSocketController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/websocket/WebSocketController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95dac206cc0df768209bc277191659c77049052b3e63c0b6f1f4a3cb623d5a46
-size 12528
+oid sha256:e6d24e1c97208ee81fe1b2874d5a0e9e16997615212c5e3eb54e7016ebf4e62a
+size 12522

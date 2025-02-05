d9de843a2609f3def9a88c7277cbe53badc6c9c5
이가희 <lgh7956@naver.com>
2025-02-05 22:44:01 +0900

    ✨ feat: [Scent] 메인페이지 디자인 수정, 캐러셀 및 모달창 구현 <이가희> S12P11A205-228
    Former-commit-id: 7f2556d715f5a5807eddd0cc47d1688c76dd2d3d

---
+++ FrontEnd/scentify/src/feature/scent/scentmain/scentImages.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cd0e9822babf61e870c4d564a80d664f1f7269a4f681dfac9fe39a9cd3be333b
+size 6022
--- FrontEnd/scentify/src/pages/Scent/Scent.tsx
+++ FrontEnd/scentify/src/pages/Scent/Scent.tsx
@@ -3,10 +3,8 @@ import ScentMain from "../../feature/scent/scentmain/ScentMain";
 
 const Scent = () => {
   return (
-    <div className="flex h-full items-center justify-center">
-      <Routes>
-        <Route path="/scentmain" element={<ScentMain />} />
-      </Routes>
+    <div>
+      <ScentMain></ScentMain>
     </div>
   );
 };

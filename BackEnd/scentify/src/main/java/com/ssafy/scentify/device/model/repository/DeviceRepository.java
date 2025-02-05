4ff7153b1bc8683efcf3eda1461f82b84ad1f527
SunJung <djtjswjd1@naver.com>
2025-02-05 16:07:30 +0900

    [CHORE] 기본향id 스펠링 오류 수정
    Former-commit-id: 18e72c94721ed5a886d6e94d96cc5b57f201748b

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/device/model/repository/DeviceRepository.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/device/model/repository/DeviceRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:803a55d7b9080f5c285d9ad55e32b219a8d38a906aad1a5378051b925bd87de0
-size 3285
+oid sha256:a8d1eeb826554b2e268cdcd2119ca627821f6cc7fe0fdde338a031a63bc15232
+size 3286

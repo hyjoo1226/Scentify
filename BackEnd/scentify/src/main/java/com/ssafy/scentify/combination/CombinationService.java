375a11de19dc43f19e660e953466f4b89f0f5980
SunJung <djtjswjd1@naver.com>
2025-02-05 16:46:32 +0900

    [FEAT] 기본향 조합 변경
    Former-commit-id: fc86db10a47394741626fef997bd0c327114af02

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/combination/CombinationService.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/combination/CombinationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9467779268e46cfb6b502edc2c486ac15d1193c18f80106cea96e4f60442ea9f
-size 1159
+oid sha256:da985a679205b6998495030f27a0fc62af8f023ce86f2e04368bbe9bf5a06b65
+size 1290
--- BackEnd/scentify/src/main/java/com/ssafy/scentify/combination/model/repository/CombinationRepository.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/combination/model/repository/CombinationRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f1bd7063e250eb485470fd212088be296ae1154f45ce21b68095bad2a622296
-size 1420
+oid sha256:941ac5da76fc96dd38387cfc56a3f1ffc4c933fcae0606571836e527d15394dd
+size 1969

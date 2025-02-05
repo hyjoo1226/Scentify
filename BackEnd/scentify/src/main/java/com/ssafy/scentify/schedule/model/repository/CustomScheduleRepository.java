c5e0821dd7ced50f3a3fe7ea5cc9f12d2e175c55
SunJung <djtjswjd1@naver.com>
2025-02-05 10:24:02 +0900

    [FEAT] S12P11A205-232 악취 모드 설정 수정
    Former-commit-id: 2982ac90bb82f74cb60f98e34d3899c76b8570e8

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/model/repository/CustomScheduleRepository.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/model/repository/CustomScheduleRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1190fde533073ba61364e9b5d111ac5b2b7bf5e08f276061172df5e683f4c284
-size 2924
+oid sha256:66e4b3f6927f5cba8dcf6ea331488ac5732f90562704d78e06305c0d8893a3c4
+size 2916
--- BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/service/CustomScheduleService.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/service/CustomScheduleService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0745114988a9dde97a578fa39eba110bb3c43a8b6011ba463e8c631f21e93253
-size 1821
+oid sha256:32bf9ddd1872eb360cbb338e97ddebcbf7a8fef52c41ccc80fadd4ec0da08624
+size 1813

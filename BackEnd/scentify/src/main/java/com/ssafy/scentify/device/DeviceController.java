25d4d6d16ab557bb07923a4f2dab1156f282b2ba
SunJung <djtjswjd1@naver.com>
2025-02-05 15:56:25 +0900

    [FEAT] 바로분사 시 RB에 메세지 전송 구현
    Former-commit-id: f34441e2307346213fd4f8e1d5244d0cf9fbca63

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13ec9abc3ab5e1d29ac90f8cec422079b4ef03274d51b49e20bb7c6a62c0d5ce
-size 12860
+oid sha256:e200dba3f804f4c1aadd4836663d8882326414743ba491580c7aec209ffe55ec
+size 12924

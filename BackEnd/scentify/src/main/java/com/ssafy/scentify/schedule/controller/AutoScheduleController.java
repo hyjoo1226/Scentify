2ec2d97686b751ac5b14f817ea6c2aad1793a65a
SunJung <djtjswjd1@naver.com>
2025-02-06 19:09:42 +0900

    [CHORE] 자정에 스케줄 RB 전송 수정
    Former-commit-id: 01fba203886d0a362b6201fb91f6f234aa72cba3

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/controller/AutoScheduleController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/controller/AutoScheduleController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d97d912c577605530e291313f7126d5ae694e214a44338dcfc3f46ef1e72c669
-size 6609
+oid sha256:73e26cd6216ea503d7d45e934407a5cd7e8792e89ace8fb69440e77a26948bfc
+size 7901
--- BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/model/repository/AutoScheduleRepository.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/model/repository/AutoScheduleRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:002f4610d01b82a6cf4c62ae85df2d35abac6e99c0fe78276999dc9dcd7b3b1f
-size 2488
+oid sha256:79b6d0c6bfa50be60d5b1f6bc90ab5d85a4d6959cdf0368f65a293990c07e814
+size 3234

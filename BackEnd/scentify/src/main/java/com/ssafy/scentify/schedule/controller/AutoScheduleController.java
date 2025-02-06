134b6f2609adad30498fa57d37d1500f3e41dc29
SunJung <djtjswjd1@naver.com>
2025-02-06 19:13:58 +0900

    [CHORE] 충돌 해결
    Former-commit-id: 3a7507ce9f30905e814574a1526f545c4cc27fd9

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/controller/AutoScheduleController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/controller/AutoScheduleController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73e26cd6216ea503d7d45e934407a5cd7e8792e89ace8fb69440e77a26948bfc
-size 7901
+oid sha256:1cfb205fc30fe5b994a202e70924ff8daa8ad617ebe141e28a24815ee2c6d742
+size 6608
--- BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/service/AutoScheduleService.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/service/AutoScheduleService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:388aa44c8cf574f69b2b0081991968403f53688ef426d159c01accb181f2acbb
-size 1702
+oid sha256:1c75545a74ae48d45cdc594db5542ac7e5092eee635a0470620b3e598d3da588
+size 1522

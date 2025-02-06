c63d588cf894e8dbe935c55a6764bf652d8e90d5
주현호 <sparrow9576@naver.com>
2025-02-06 16:50:23 +0900

    ✅ add: [Control] 유저 디바이스 스토어 연결 <주현호>
    Former-commit-id: d7bffd090d135322be4f671c8f0befa6b9effa84

--- FrontEnd/scentify/src/pages/Control/Control.tsx
+++ FrontEnd/scentify/src/pages/Control/Control.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:081c918f9d93a543d3a0435f3e2083d7af48dfd686c64a9a77c650135c6991c3
-size 7857
+oid sha256:9a0c6e8463090eab8bbe02c49b680ab8c9576d18fb48977adeae6c6a5ccad82e
+size 8000

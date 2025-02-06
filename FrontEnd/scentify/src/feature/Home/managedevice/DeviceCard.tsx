04b7269e077adc1e370d8eac28b1fe5dd5921a54
leegayoung <leegy0720@naver.com>
2025-02-06 14:57:14 +0900

    ✨ feat: [home] 디바이스 삭제 API 연동 <이가영> S12P11A205-277
    Former-commit-id: 5f09ab191e7dc9c16c6c3f1324b6249bc47ed579

--- FrontEnd/scentify/src/feature/Home/managedevice/DeviceCard.tsx
+++ FrontEnd/scentify/src/feature/Home/managedevice/DeviceCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d10a60401789629837d1f3a195b23b5fb1710f21df25c1d317c0db30a76ed91
-size 5524
+oid sha256:832f6a631d81fd1d450a7561e71e080c6eb620372f95ca0b855bdd8cb4d8458a
+size 5667
--- FrontEnd/scentify/src/layout/Header.tsx
+++ FrontEnd/scentify/src/layout/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cec5c37a66e50a46055a3497412f233abb6e94a0a1f66a079637834ff3996118
-size 2265
+oid sha256:91f849a56b1705ec5c7bcdc859bcd49920519a6f1677e983b78cf61ba6e7d11c
+size 2350

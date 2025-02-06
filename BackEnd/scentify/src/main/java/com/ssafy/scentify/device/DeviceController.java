5f7f73d971705222cfc398a9a310a22b7e083486
SunJung <djtjswjd1@naver.com>
2025-02-06 15:16:53 +0900

    [FEAT] S12P11A205-274 RB 핸드쉐이크 종료 요청 메서드 구현
    Former-commit-id: 1d302a00c6736515914959993b25163fbedba89f

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d95bfaca63134bc1a6c5f634015e07c3d51541cf0abdbcb5f3a82a03b8c2f024
-size 15056
+oid sha256:c94b115b6aa997148b66f87131d69e214902205dc588fad6707fc5b61efda4d5
+size 15805

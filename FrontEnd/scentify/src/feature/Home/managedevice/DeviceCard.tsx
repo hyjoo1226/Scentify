190f815b4829cbfbd94c534707c4ca33accfd78e
leegayoung <leegy0720@naver.com>
2025-02-06 15:54:26 +0900

    ✨ feat: [home] 대표기기 설정 API 연동 및 기기 컴포넌트 지역 상태관리와 렌더 반영 <이가영> S12P11A205-277
    Former-commit-id: 6eced12abe1fa6a2f61ba7ee61d6272827e033d1

--- FrontEnd/scentify/src/feature/Home/managedevice/DeviceCard.tsx
+++ FrontEnd/scentify/src/feature/Home/managedevice/DeviceCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:832f6a631d81fd1d450a7561e71e080c6eb620372f95ca0b855bdd8cb4d8458a
-size 5667
+oid sha256:94074cbdaa4a09a325d3434d6e685fa62e59e5805af815b2e49f91268d14c524
+size 6071

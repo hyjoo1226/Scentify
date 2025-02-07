c59b2166a2c188145078a60ed39587812bead3da
SunJung <djtjswjd1@naver.com>
2025-02-07 12:37:54 +0900

    [DEBUG] 디바이스 삭제 보완
    Former-commit-id: 6133a90c944d174fba2a864375175c4b7175fa34

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6662d79b3a7c7045184541de2d7f76fd77a357ef183b90538c33a4818b11e51e
-size 16032
+oid sha256:90c14ff0163818c6802438430ca6d5b8623d49d48edd4a79fcaa0dabe6bddcee
+size 16069

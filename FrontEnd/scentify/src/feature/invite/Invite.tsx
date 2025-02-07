d689cfa085dabc0cc6f2055d4720c358ccfa922d
leegayoung <leegy0720@naver.com>
2025-02-07 14:18:55 +0900

    🐛 fix: [home] 기기가 없을 때 메인 홈에서  에러 뜨지 않게 수정 <이가영>
    Former-commit-id: 442fd7f49f1f61f70f58151a8b73b8ada63c86f6

--- FrontEnd/scentify/src/feature/invite/Invite.tsx
+++ FrontEnd/scentify/src/feature/invite/Invite.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae201625ab4bb5c3565c212a196206d17060e3239679272ab669b6bd8b7dec12
-size 2228
+oid sha256:fb6cddda02dc18a5f6a51d1acdf087999c05ef9a38fac8df22f64a3883e55006
+size 2493

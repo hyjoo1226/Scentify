78ff1faafc6d1f85b27fab1dc16953815fa361fb
SunJung <djtjswjd1@naver.com>
2025-02-08 21:43:23 +0900

    [CHORE] 기기삭제 시 디바이스 리스트 size 확인으로 수정
    Former-commit-id: fba9c4814c521c93b48ba24216987f6eb24516ff

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:901e9578fae31c390dc1898ca310d142c5dd86ca76247c5498ba93f72516e229
-size 16196
+oid sha256:4d8ac37549b23b90b63e8c4d51eebf5e4f1fa0e3b0858e50ea96ca71f4aee9ac
+size 16199

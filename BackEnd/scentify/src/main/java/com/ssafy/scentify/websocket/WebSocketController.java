808004c4aa4e04a9e21fcfaeb0570967984e6f4a
SunJung <djtjswjd1@naver.com>
2025-02-08 21:54:47 +0900

    [DEBUG] 웹소켓 combination 요청 메서드 수정
    Former-commit-id: 50da5ed7bf0db80140272a07fafaa542a6b68746

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/websocket/WebSocketController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/websocket/WebSocketController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58c96725f397ce1fb255c7bbe23d7f0ed80481a71f8dd945ab1af9d80afeece4
-size 9082
+oid sha256:6dc99027420c5f3725b757fc2e320b9f14a75993d694b53d770ed4967c773317
+size 9106

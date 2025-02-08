78aa3a08d17e24b69fc8272efb25ca826de10136
SunJung <djtjswjd1@naver.com>
2025-02-08 22:19:48 +0900

    [CHODRE] 빌드 테스트를 위한 파일 수정
    Former-commit-id: 252173562d77d3f2d65ed207b292c63d3baa1c37

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/user/controller/GoogleController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/user/controller/GoogleController.java
@@ -1,5 +1,10 @@
 package com.ssafy.scentify.user.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/v1/auth/google")
+@RestController
 public class GoogleController {
 
 }

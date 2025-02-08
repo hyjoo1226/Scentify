afe12f651f7b269e6260272ffd5e3bda2a5c542f
leegayoung <leegy0720@naver.com>
2025-02-09 00:38:29 +0900

    🐛 fix: svg 및 타입스크립트 빌드 에러 수정 <이가영>
    Former-commit-id: a97dc75c4b2b88d102cc9656b1a22e046b6c319c

--- FrontEnd/scentify/vite.config.ts
+++ FrontEnd/scentify/vite.config.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e7ecf34e8123b60c43741c57e0ad931d82ea301e44510a545259452fd7c2051
-size 736
+oid sha256:452b10bf5fce203ee1cfa0fc8eddb1cfe9e37a01bb94f1e1b494b9972464adba
+size 1014

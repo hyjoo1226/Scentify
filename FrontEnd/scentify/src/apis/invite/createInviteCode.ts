6c12e39a26ffa00c50152e3f7c2a4f2a1c4f5b87
이가희 <lgh7956@naver.com>
2025-02-09 13:13:03 +0900

    🐛 fix: 그룹관리 무한렌더링 수정<이가희>
    Former-commit-id: c654fc50e5c60677a81d520ea5a7e92385323aaa

--- FrontEnd/scentify/src/apis/invite/createInviteCode.ts
+++ FrontEnd/scentify/src/apis/invite/createInviteCode.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a8bde232593e25fe1b83523da5c1237eafc5c496857c73a3e36a2e0126b9409
-size 1624
+oid sha256:aa68cc77e41bf88e543e57971fb72aa029fff012f3ce9d6deef497512fc49a81
+size 1830
--- FrontEnd/scentify/src/feature/my/components/GroupList.tsx
+++ FrontEnd/scentify/src/feature/my/components/GroupList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0a1a7ac62a65fce1475a31311dbe512179b4272b8717660450d50b9cb7e13d1
-size 7685
+oid sha256:a833a25bfb93762dd8910d7ca268f0a98d2482684ab096a805c18b22e11bb433
+size 8445

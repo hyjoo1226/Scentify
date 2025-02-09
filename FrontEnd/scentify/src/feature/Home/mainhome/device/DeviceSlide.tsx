b075f7c1b6de9ffdc4b3eebb3f1aaa1bd61c1de7
leegayoung <leegy0720@naver.com>
2025-02-10 00:25:10 +0900

    ✨ feat: [home] 대표기기를 항상 슬라이스 첫번째 위치하도록 수정 <이가영>
    Former-commit-id: 8de8f3f7d9135bf301595bfdc4772f481a9e23d8

--- FrontEnd/scentify/src/feature/Home/mainhome/device/DeviceSlide.tsx
+++ FrontEnd/scentify/src/feature/Home/mainhome/device/DeviceSlide.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aed27658fb16b97a44153e9ea31a88c378b814ae4a6cd36d24191452d2598bb1
-size 4715
+oid sha256:3a555dab9c608aa44e6cd0929bcc9761809cf02743c4b68fddaa2cda42b1ff1c
+size 4660

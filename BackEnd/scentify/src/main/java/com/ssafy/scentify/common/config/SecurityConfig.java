0ba71fe5d1025887f7e81e5be5190773e950a4f5
SunJung <djtjswjd1@naver.com>
2025-02-09 14:15:06 +0900

    [CHORE] 스프링 시큐리티에 구글 로그인 허용
    Former-commit-id: 90372f7e0e6518bd6c961896cd41fac5f5c2f75e

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/common/config/SecurityConfig.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ecf6bb42058d3b4457045103c42027346789e9991ca7aa13bd621c04a9cea98
-size 3401
+oid sha256:30bc95285de719d39690a1b0e5c7bd0985ddf2626af33dbea659b10741d6879f
+size 3466

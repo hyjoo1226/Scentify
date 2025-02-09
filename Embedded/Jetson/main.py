da6d3a87abc9eee0e2b10b5602e6f4b609880b8c
ath <seof622@naver.com>
2025-02-09 18:40:38 +0900

    feat: add auto mode logic when auto mode change
    Former-commit-id: cd9a45a41e8d49bb89c67d2ab2013e798e5a4984

--- Embedded/Jetson/main.py
+++ Embedded/Jetson/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5bfe860e827582d48b2ff441fc301fdb28e30b40f870754714935d1c30182b6b
-size 8657
+oid sha256:99752ca035ccf9a9c748a2207aa532fe77f222d305048ed84da4cabaf8f2e42a
+size 9764

e8ff383c58da6875f6b71e3966545be4c38ad31f
SunJung <djtjswjd1@naver.com>
2025-02-09 21:19:41 +0900

    [REFACTORY] 그룹 멤버 삭제/ 그룹 삭제 로직 보완
    Former-commit-id: e1ff63f3a44e424b5fac3d66d75e8e7de6caa458

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/group/model/entity/Group.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/group/model/entity/Group.java
@@ -1,21 +1,3 @@
-package com.ssafy.scentify.group.model.entity;
-
-import lombok.*;
-
-@Getter
-@ToString
-@NoArgsConstructor
-@AllArgsConstructor
-public class Group {
-    private Integer id;
-    private String adminId;
-    private String adminNickname;
-    private String member1Id;
-    private String member1Nickname;
-    private String member2Id;
-    private String member2Nickname;
-    private String member3Id;
-    private String member3Nickname;
-    private String member4Id;
-    private String member4Nickname;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:33c2415006eafa02856a292131ff28c179da5d5931292db018c0fec3c8c815b3
+size 525

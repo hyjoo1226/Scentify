04b5ce253a9127b6a3d2f720609087e7ebc6796e
ath <seof622@naver.com>
2025-02-09 19:24:57 +0900

    feat: add dummy data
    Former-commit-id: 9649cab53eebbbd761eed366beff3ba20ac0ef2d

---
+++ Embedded/Pi/dummy_data.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dbb9d85823737980f0ede432e58dca40c59b462df12bb95055f1507741382d93
+size 2265
--- Embedded/Pi/main.py
+++ Embedded/Pi/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c52ba35441bedbc55a3ac7306841bb68040325061d5baddad49b19900956e8ef
-size 2544
+oid sha256:e679e9832a8d6fb8b844107c189d75faf38e24c9b84b8fedd0b3fade6bbbde36
+size 1621

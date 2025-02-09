376033ee2c97d813008ef236b1d6192cfc9e1724
이가희 <lgh7956@naver.com>
2025-02-09 23:07:26 +0900

    :bug: fix:[My] 그룹관리 멤버리스트내  본인제외, 삭제, 그룹삭제,초대하기 버튼 관리자 계정에서만 띄우기 <이가희> S12P11A205-282,S12P11A205-289,S12P11A205-281
    Former-commit-id: af0c3fe175f6c684890cc865a13eebca8ac0b043

--- FrontEnd/scentify/src/feature/my/components/GroupList.tsx
+++ FrontEnd/scentify/src/feature/my/components/GroupList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86fe55a6cd9774d198494ea64e9f28c80047507cbad81fef68b202d1ec2d4338
-size 9825
+oid sha256:56e4b5f9212bea09c260b10ac984b60db2b6a88ddb24e65d48c08ec307d3e9bd
+size 9948

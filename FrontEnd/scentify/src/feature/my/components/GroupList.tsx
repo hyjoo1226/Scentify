845862f71207be5f3da49a8d24150a8ebbd47115
이가희 <lgh7956@naver.com>
2025-02-10 01:23:46 +0900

    🐛 fix: <My> 그룹관리 내 기기선택 드롭박스 왕관 수정 <이가희>
    Former-commit-id: a446004fbc75dbe9080188eb63e0f485180eda12

--- FrontEnd/scentify/src/feature/my/components/GroupList.tsx
+++ FrontEnd/scentify/src/feature/my/components/GroupList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00ca91062ec368c0cd70f2c04da7e62b4b56cb16153f9b81d92c42de90c21091
-size 10145
+oid sha256:e67033da3cdc68ceb42c72c3ab77a46005babf112621c153e101d73949148062
+size 13526

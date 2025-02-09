7d3452389448d713b097119f8673fa4e7a23323c
SunJung <djtjswjd1@naver.com>
2025-02-09 10:30:29 +0900

    [CHORE] 배포 서버로 카카오 리다이렉트 주소 수정
    Former-commit-id: 53626df530e97e42db5355919c5e67b43c7823db

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/user/controller/KakaoController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/user/controller/KakaoController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d84fc039e06a75aae6a05d9349ae80100c07a98bd92e4a8a87b2d7d6996bff1c
-size 11221
+oid sha256:7d2221d5c0799ac5c6c1879811e9fc809de26b151e6abc7b7d7800fc946f98a6
+size 11244

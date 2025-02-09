88fb22425c4da225ca2ba2cb95b067387e161a35
ath <seof622@naver.com>
2025-02-09 19:26:05 +0900

    refactor: comment request destination
    Former-commit-id: 0eca44ba24e354bdfa8cb673f6a7aff9abbc18e4

--- Embedded/Websocket/modules/client/websocket_client.py
+++ Embedded/Websocket/modules/client/websocket_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:919a15b1e752d4676bf89dcb21cf54bf8c06a225eb1b8b2f3f9407cde81df46b
-size 10896
+oid sha256:3d181cb30e1ec49f2cbe0fe00baa709f7af2b9890c2b93087bea257c32220a12
+size 10898

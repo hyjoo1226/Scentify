5b6349d070d3fade32e02354f9bea05a91b2777a
seof622 <seof622@naver.com>
2025-02-10 08:31:25 +0900

    feat: add print log variable and condition logic
    Former-commit-id: efa054d8ef5ad0d65db65bbdcd9a9ba2857f3c3b

--- Embedded/Jetson/main.py
+++ Embedded/Jetson/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2463f8dfeef75cdddfe9b77ef9370112b1784f5d42329561e116f2d21abc5502
-size 11168
+oid sha256:7d5d48b1ce6ddbb34b8c7e01600eee3d153dbafbcb6ba8e447b6c2b42f6d8667
+size 11525

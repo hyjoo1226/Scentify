f92bdc2d1f764afee54e8f6977a16ebea7f8202b
seof622 <seof622@naver.com>
2025-02-10 21:17:43 +0900

    refactor: change dummy data
    Former-commit-id: 948bb82a22d367d6ab2ccf46ff7ded0115cb7928

--- Embedded/Pi/dummy_data.py
+++ Embedded/Pi/dummy_data.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbb9d85823737980f0ede432e58dca40c59b462df12bb95055f1507741382d93
-size 2265
+oid sha256:df5c59dfb3423943427fbc6766dc152617b7ed77dc3830ce41612eb3c635888f
+size 2404
--- Embedded/Pi/main.py
+++ Embedded/Pi/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e679e9832a8d6fb8b844107c189d75faf38e24c9b84b8fedd0b3fade6bbbde36
-size 1621
+oid sha256:6493c4eae0daad1792bcebabcc3138ab021d9c5fc94a8a8f5eccdbb69b152db4
+size 2304

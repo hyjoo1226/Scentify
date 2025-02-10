8aa1cd32a9469306758e4bf8859eeac1e23ba7b3
seof622 <seof622@naver.com>
2025-02-10 11:28:13 +0900

    feat: complete testing auto mode operation
    Former-commit-id: 272210e3ba7f63192a5e10c5d80309fa1a64995b

--- Embedded/Jetson/main.py
+++ Embedded/Jetson/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bdd3e8d01660c3e1914a408c68af195f519da89681c055d6b5908405d563a29b
-size 11532
+oid sha256:7c42346ba20037623207cbc8f48fea5322cf08cf4a72eceab5556be3603b5052
+size 12637
--- Embedded/Jetson/mode.py
+++ Embedded/Jetson/mode.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:880d405345297e1c22e23e05c228c1863c529ee06503fb559575ab91fe6f5fa6
-size 577
+oid sha256:3e6df666298be2797d66683abc1151c3c2c6dd4b97a6b0bb557308e0c86d46c6
+size 613

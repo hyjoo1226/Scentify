7ed0e34568c66d602855ebebf649299bb84fb256
이가희 <lgh7956@naver.com>
2025-02-10 13:53:23 +0900

    ✨ feat: [User ] 비밀번호 찾기 UI컴포넌트 구현 <이가희> S12P11A205-305
    Former-commit-id: 932aef1754dd05e3ceae04a3be0b9ad3704493d8

--- FrontEnd/scentify/src/feature/my/components/MyUserCard.tsx
+++ FrontEnd/scentify/src/feature/my/components/MyUserCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:572cd3632254ac723cc4e99e7f417ee361848d1cd593148e31d304fa90ce72ea
-size 4273
+oid sha256:abb6cf70499ed634d00b1e3fd69db4742eda7c4192a1a02ed5e6f796038452a2
+size 4220

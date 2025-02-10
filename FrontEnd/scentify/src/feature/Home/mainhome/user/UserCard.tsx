a15b622579babf4a5a49a4c91e1a801b72724bca
leegayoung <leegy0720@naver.com>
2025-02-10 16:03:32 +0900

    🐛 fix: [home] 유저프로필 이미지 로직 업데이트 <이가영>
    Former-commit-id: be18b9f3b2545f8fecdc2d87d15f36c38458ac54

--- FrontEnd/scentify/src/feature/Home/mainhome/user/UserCard.tsx
+++ FrontEnd/scentify/src/feature/Home/mainhome/user/UserCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:181c9f96f342288c401bdb9e6692c2d686b1ec5f1198a4bd3a3a5a77c2711583
-size 5374
+oid sha256:12176dd798768df1d4d05805e8f94dd2f835610a86be632e60d5237fffa85211
+size 5334

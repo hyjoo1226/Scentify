2e183fa15ff76306fa434ef6149c79731285c955
seof622 <seof622@naver.com>
2025-02-11 08:07:47 +0900

    feat: add stink sensor threshold value
    Former-commit-id: 3f7f00e91cd087c28eab68ba11d0a2d6f22db3e8

--- Embedded/Jetson/main.py
+++ Embedded/Jetson/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4951193ed130b23de23044e26b314706ac9a898d2003ea10fb390b609d6c9dbb
-size 13393
+oid sha256:a2c876ada4e4c739f3db921aad46a00a3c2b53b91a378f5ed729d0a3188c0568
+size 13111

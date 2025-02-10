746ea150abab3fc6a3296d3f6e48dfea08b6c873
ath <seof622@naver.com>
2025-02-10 23:58:00 +0900

    feat: make hanlder for schedule mode
    Former-commit-id: 20cc5524829a6276c8d8a2d0df4e683449cde24a

--- Embedded/Websocket/modules/client/websocket_client.py
+++ Embedded/Websocket/modules/client/websocket_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb3df456c82575117fc76bc42177cc9cb15ba0ca44036517c1ff276ccb2317e5
-size 11476
+oid sha256:3c1cd412e2424dbff02be73a845913166759bbbde4629833eb202d9fb6625151
+size 11497
--- Embedded/Websocket/modules/client/websocket_response_handler.py
+++ Embedded/Websocket/modules/client/websocket_response_handler.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a568dadbb6d7c1ee52a65be0536701916dadc233a13073e2bdc4f36283582ef3
-size 3880
+oid sha256:818baf003d9df16b67f04c99cdee2291b292fcd8307dfc446346fa5bf9dd5f3a
+size 5467

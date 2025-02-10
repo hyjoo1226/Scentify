3b82e39aec9e81252215f79f8b3a67aea03ee808
seof622 <seof622@naver.com>
2025-02-10 09:40:18 +0900

    feat: change send temp and hum condition
    Former-commit-id: bd83e4417b0dcd6576802e028eb82a68d6128fb6

--- Embedded/Websocket/modules/client/websocket_client.py
+++ Embedded/Websocket/modules/client/websocket_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d181cb30e1ec49f2cbe0fe00baa709f7af2b9890c2b93087bea257c32220a12
-size 10898
+oid sha256:53e88cfc1663b7dbe0301b554d07e4bfd91f24f02f46ec5f731fa6517484381a
+size 10962

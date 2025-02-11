d36bd5f7cade15e31fabb3a1ab3739ec29fa906d
SunJung <djtjswjd1@naver.com>
2025-02-11 12:48:41 +0900

    [HOTFIX] 찜 삭제 로직 수정
    Former-commit-id: eb8fa10312abf8553e5e087885210c58fc27b462

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/favorite/FavoriteController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/favorite/FavoriteController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:814a6acfd8eec0f5c4d950047a9e65a0150fba15e9896e456da1704746a10a4f
-size 9102
+oid sha256:c95ad960a81667b84e447e4d406c9483c8046de814b7e6c0a6f0fb7d9a8ff7ce
+size 9132
--- BackEnd/scentify/src/main/java/com/ssafy/scentify/favorite/model/repository/FavoriteRepository.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/favorite/model/repository/FavoriteRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcf0685c204c109867a58748ba576c1e05f0b554287c07469169811c7dfabc85
-size 1289
+oid sha256:ead15da359498d1d59a0bc09b380f01987cc355ef2dd2add4aa5b59c9952faf2
+size 1323

fce2d079836f193a72f763742f1f8e1e350784ff
leegayoung <leegy0720@naver.com>
2025-02-11 13:53:28 +0900

    ✨ feat: [home] 구글 소셜 연동 <이가영>
    Former-commit-id: 730e2ea3d93ef1b78caca33704ea2de1e2e48209

--- FrontEnd/scentify/src/components/Social/SocialLogoBtn.tsx
+++ FrontEnd/scentify/src/components/Social/SocialLogoBtn.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7c6193bd9eff8f15eb48da0d950990a10c402b0186f4c20977e4aa888669c0d
-size 888
+oid sha256:579d9d4dc7ef93cb66fde07f7e6902aa42932ff5e9af3acf6457b444e7388044
+size 1136

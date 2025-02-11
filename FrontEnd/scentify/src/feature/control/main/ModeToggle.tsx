befdfa930ac52a7f50dd2b45eb43e44902a416d8
주현호 <sparrow9576@naver.com>
2025-02-12 00:53:27 +0900

    🐛 fix: [Control] 모드변경토글 초반 깜빡임 해결 <주현호>
    Former-commit-id: 705be932348f1fc0c7dc99759e8dff5206338da1

--- FrontEnd/scentify/src/feature/control/main/ModeToggle.tsx
+++ FrontEnd/scentify/src/feature/control/main/ModeToggle.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcacdb835e137d6836937484a6069333e95b77a43df662a1715ce1d84bdd88bb
-size 938
+oid sha256:3685a2e61f95856b95a01aed57a000d051532b2c13d63fedc7d524850732124e
+size 984

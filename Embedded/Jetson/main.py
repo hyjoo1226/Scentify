5e843c3f2774ba37b5362d4b62eeed0f2f7a5866
seof622 <seof622@naver.com>
2025-02-12 18:28:52 +0900

    feat: run auto mode when auto mode data receive
    Former-commit-id: f45b3c22103c4dd1fe9a4fb6674be5aaa019e538

--- Embedded/Jetson/main.py
+++ Embedded/Jetson/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cd586043242b74d35989e26bdfaffbde7087ca81f3da0f44daeaf6d299d907d
-size 15363
+oid sha256:c1df7ca1445d8360ed623f5f64eb26b2ab928b9f476aeac7317e37343bb19cf6
+size 16867
--- Embedded/Jetson/mode.py
+++ Embedded/Jetson/mode.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35389930fb9902fbeb64ab5e44bc0dcbff60f493c2de0ff96bcac96bef424465
-size 614
+oid sha256:8f4e9adddd985f7705f4872dde45b0090f0c2473f9db05d591b6cedb1652b22e
+size 644

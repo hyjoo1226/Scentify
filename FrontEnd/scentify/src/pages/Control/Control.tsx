66e1b4535cf78b9a6e14dc780f12bf4b66c4fd93
주현호 <sparrow9576@naver.com>
2025-02-12 13:31:45 +0900

    🐛 fix: [Control] 기기변경 시 모드 표시, 현재 모드와 같은 토글 클릭 시 동작 방지 <주현호>
    Former-commit-id: e200c101a9c51d0b60ce88e59dea095e2f9d02bf

--- FrontEnd/scentify/src/pages/Control/Control.tsx
+++ FrontEnd/scentify/src/pages/Control/Control.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fca644e5476d6d4323b72f13ad4045640197f82182d3508188d01752d5d7e5fb
-size 10800
+oid sha256:9845c2fcde18a75ff384a061c6435c1bbb03c7c0cc4f644927b2e96e4f96403e
+size 11053

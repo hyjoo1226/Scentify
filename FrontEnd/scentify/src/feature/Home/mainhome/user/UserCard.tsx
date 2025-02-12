a7fab1673296a7f8ac607d33970c85777895a057
이가희 <lgh7956@naver.com>
2025-02-12 12:46:39 +0900

    🐛 fix: [Home] 날씨 API호출 에러 해결 <이가희> S12P11A205-313
    Former-commit-id: 8e9c01c7ff21f6c176fd23be0a5ea7f6ad186f97

--- FrontEnd/scentify/src/feature/Home/mainhome/user/UserCard.tsx
+++ FrontEnd/scentify/src/feature/Home/mainhome/user/UserCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22436d260dbe5f0827709600b0386f90395f3a6cfe6052ce7d723b22bf2eaca3
-size 5398
+oid sha256:77bce7839e177e0a2b37994a676c4adfc2df6b8448d9a033f0289cf806c985d7
+size 6427
--- FrontEnd/scentify/src/feature/my/components/EditPassword.tsx
+++ FrontEnd/scentify/src/feature/my/components/EditPassword.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:699360872cf8eefb8a947aecbfd9e897e011caa07ad60d476dce4a41b7b47956
-size 5946
+oid sha256:d6d9c0bd13427a40b5f6201f8117aedba03acee5e48218f5d05dc2a192b8acf1
+size 7266

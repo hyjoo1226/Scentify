1533833a72e50ed081cd4314de5413da4232ec03
SunJung <djtjswjd1@naver.com>
2025-02-12 12:41:31 +0900

    [CHORE] 라즈베리 파이 응답 map 형식으로 수정
    Former-commit-id: 40f1ef9280ad144ebe78aec38eb5d571be822614

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3694358e9f4b95cedf0b6ec78b254f782d751ecdab0f16d388225edd5022eadb
-size 18150
+oid sha256:cb3705fe3ba5ace61460b737947ccbb1a7cb1bed6013bd5171e9d2b364a933a4
+size 18124

47a7ea15de99a2f70d283e6ed071c7298289e83e
leegayoung <leegy0720@naver.com>
2025-02-12 17:18:06 +0900

    🐛 fix: 전반적인 디버깅 출력 콘솔 1차 삭제 <이가영>
    Former-commit-id: f6650dc7bc40d3d220e79e152d9b36f20406aed2

--- FrontEnd/scentify/src/feature/Home/mainhome/device/components/DeviceSchedule.tsx
+++ FrontEnd/scentify/src/feature/Home/mainhome/device/components/DeviceSchedule.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c645b6d8758e7cbab47d6eda974365aae8b5393e0a0544b969c60071548b281e
-size 7139
+oid sha256:3a0ad07038a2d644043bf8ff5d6231f0a23d9e7ecac645e4335aa15c7c9ae592
+size 7078

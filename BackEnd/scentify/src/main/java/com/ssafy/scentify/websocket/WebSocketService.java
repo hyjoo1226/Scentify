6f39312dae69ca11220023bb2fc8423b89fcd53a
SunJung <djtjswjd1@naver.com>
2025-02-12 13:22:07 +0900

    [DEBUG] 스케줄 수정 시 RB에 데이터 보낼 때 에러 해결 시도 2
    Former-commit-id: 5227aa83ca382af87b99f1c9352076352ac19a58

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/websocket/WebSocketService.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/websocket/WebSocketService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1343c8cac36022f3d409304b491295a66f0cdbfc1d1004f61a617da27b2b1d3f
-size 9252
+oid sha256:489735d5ed2dadb99bbc373efb4016990e2b70c9986d4cda760e5fcef66ebf9e
+size 9218

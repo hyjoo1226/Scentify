bf0c375fb07a5176eff324177935f84f36a9c90b
SunJung <djtjswjd1@naver.com>
2025-02-13 15:30:12 +0900

    [CHORE] day에서 0 허용하지 않도록 보완
    Former-commit-id: d93b5b81009324a5158f30c28d7ad170769619aa

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/model/dto/CustomScheduleDto.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/model/dto/CustomScheduleDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b4dd1d796a2e10dc4e7c33ac09e640cf7fcbd3c66e67cc2b2217eb219dc760f
-size 2179
+oid sha256:858b01df73b0432325df738073583946cc5c0e371890e90fd45ca9b8bf87daf5
+size 2181

e670a3db4c40acdf9d26f274643acac94e9a983c
leegayoung <leegy0720@naver.com>
2025-02-13 14:52:42 +0900

    ✨ feat: [home] 기기 캡슐 수정 - 기본향 수정 API 연동 <이가영>
    Former-commit-id: 4c9728ef1afde2a519a190cf7c7bd581081f813e

--- FrontEnd/scentify/src/layout/Layout.tsx
+++ FrontEnd/scentify/src/layout/Layout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eae30fe87a95f5f751c764b06d1c51249120132c45b0c2722752854877ab70ff
-size 4477
+oid sha256:e3dd3f14638dda8302c131971396b0c0d2019bad9a1ef72b6e5aaea4fc8bcf9e
+size 4549

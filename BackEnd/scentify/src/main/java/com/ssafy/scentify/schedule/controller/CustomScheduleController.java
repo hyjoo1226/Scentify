7602e67b1bfd79de61d18780065576402dee73a1
SunJung <djtjswjd1@naver.com>
2025-02-13 15:51:43 +0900

    [DEBUG] 자기 자신은 제외하고 수정하도록 변경
    Former-commit-id: def3ad336096e65db05a5038726b3d887c62233f

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/controller/CustomScheduleController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/controller/CustomScheduleController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0771b26111ee20efbf81ba082cbc25e8745e50dd2a74b7c96cf4d99ad8e7e034
-size 12053
+oid sha256:c0f9257f92643a514493184c434ae641829d580ecc2095e23e7c6cad533caeda
+size 12149

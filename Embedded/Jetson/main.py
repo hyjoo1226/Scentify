9dd6dbab14a43c74e0c31023575cfcdf133e6654
seof622 <seof622@naver.com>
2025-02-13 20:52:43 +0900

    feat: S12P11A205-187complete test schedule logic
    Former-commit-id: 05f8c4646871676fa53222427f8417ae6a073084

--- Embedded/Jetson/main.py
+++ Embedded/Jetson/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1df7ca1445d8360ed623f5f64eb26b2ab928b9f476aeac7317e37343bb19cf6
-size 16867
+oid sha256:8d96ebd2800a5c79797d6fbeb5bf27d179114a134012c18f2c63109f67afb620
+size 18339
--- Embedded/Jetson/mode.py
+++ Embedded/Jetson/mode.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f4e9adddd985f7705f4872dde45b0090f0c2473f9db05d591b6cedb1652b22e
-size 644
+oid sha256:8d771781211d8cb0327828c3c56114a50486648019e7987e68bea63cc15d8729
+size 645

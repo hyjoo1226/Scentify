fa70b08d85aaa1765ef4e1609a56e0110dec22a6
seof622 <seof622@naver.com>
2025-02-13 21:13:32 +0900

    feat: change get frame method
    Former-commit-id: 5bd2739b7cc43b7e62fc5a12162557dd22b4bf4f

--- Embedded/AI/modules/camera.py
+++ Embedded/AI/modules/camera.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39ecaef56f37c38971257e8a61203e5a7871c362839706924c688b931ae1f599
-size 1010
+oid sha256:b6fd0df8e4338d31c28d61ef0c0d10027f49db482a30407fe4fcf50cbe0e85e5
+size 1310

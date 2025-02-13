0bce5b01b3b385b16ab28b5b1affe3b3a37243d1
seof622 <seof622@naver.com>
2025-02-13 20:51:37 +0900

    feat: divide operate function
    Former-commit-id: 177d734369308435eefec0014bf59b9700c80365

--- Embedded/HW/modules/Solenoid.py
+++ Embedded/HW/modules/Solenoid.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5f3a82d048e9747595cd457de2e719d9c5bb41f0398e02f780e64b02f13d064
-size 1048
+oid sha256:6bbaa4bebfb81c65d38932dd2f2cfe0d4d66c25308d36e014ee684c5de3a1785
+size 1157

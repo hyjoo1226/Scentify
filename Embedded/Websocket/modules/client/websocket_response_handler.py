2768541b8f39cb350728aa59f423304db31022e3
seof622 <seof622@naver.com>
2025-02-14 11:27:00 +0900

    feat: add websocket disconnection logic
    Former-commit-id: caf9fde7dfea8ac7a2648836486e53d63859bc69

--- Embedded/Websocket/modules/client/websocket_response_handler.py
+++ Embedded/Websocket/modules/client/websocket_response_handler.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fef9dcf74a345bf833bf050891871bfc77c3a02580596dbb7d0e7aa2b4682db4
-size 12243
+oid sha256:4d7d68a79c7b7612c87320055fce5a9c2937a116d0424ab76f58bfba923f2ce0
+size 12612

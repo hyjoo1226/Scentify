5cb23c49bafddd954514e04784450ed743a659f2
seof622 <seof622@naver.com>
2025-02-14 13:46:17 +0900

    feat: add hanlder topic update logic when websocket reconnect
    Former-commit-id: b85444068e9198ce86a4c9949ba27302e990a31f

--- Embedded/Websocket/modules/client/websocket_client.py
+++ Embedded/Websocket/modules/client/websocket_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aae4a8d19963b941245dca4624e96d54758a4e72cc7f760153f79bc50164ecec
-size 12840
+oid sha256:da6d764e4f46b5e83d09c832e78b434d2b056bb1724f46577a06177d1e1ac223
+size 13329

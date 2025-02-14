59f8fb9e0eebb831f7977c84db8bef92c199769b
SunJung <djtjswjd1@naver.com>
2025-02-14 11:27:12 +0900

    [CHORE] 핸드쉐이킹 주석 해제
    Former-commit-id: ce7d2657b69ce4d9f68e3e086fffc9d6e3ddcb72

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eec4b5aa4e4e62aa7e00ab36e40acd3a3ca29fcc2f728e491924efe5f7fe6969
-size 21977
+oid sha256:d277359b749489757756271887147dcebbbf554b1d5766acf32cc628d2d44b36
+size 21949

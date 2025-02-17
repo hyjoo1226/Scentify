a0615ad85119e985f6714035c05b2a7786097efe
SunJung <djtjswjd1@naver.com>
2025-02-17 16:21:55 +0900

    [HOTFIX] 커스텀 스케쥴 수정 로직 보완
    Former-commit-id: 085aa6ceb1f4b5c26ea3f89df5d1d4bc9bfcb7be

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/controller/CustomScheduleController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/controller/CustomScheduleController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:794d9cdba0345cf79bed8d0d4c2c8c61e10b9ab514960c993ca7b7fc68e29266
-size 12369
+oid sha256:d908f180c1e1223e86eb17431d93e1fd16bcb802494299b759ae9ff5c9204adf
+size 12347

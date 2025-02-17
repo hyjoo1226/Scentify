1e1214b83a7c29d0a4e00601b8e3eaaf074b8a6d
이가희 <lgh7956@naver.com>
2025-02-17 12:28:14 +0900

    🐛 fix: [MY]  UI및 오류 수정 <이가희>
    Former-commit-id: 57fc844f1e6a4527beaf5622a09ee4f6b11efb67

--- FrontEnd/scentify/src/feature/scent/ShareFavorite.tsx
+++ FrontEnd/scentify/src/feature/scent/ShareFavorite.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d8d3affca02c763f5b0e7075952a280d0bb1cf5682839e4840cc3dddf5da832
-size 7894
+oid sha256:c331b0e2f0fd7708f84cd4fa657b39db5616ab4113b26711b96fa659114a2427
+size 7988

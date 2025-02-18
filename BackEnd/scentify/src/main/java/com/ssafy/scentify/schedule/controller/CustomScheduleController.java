e1c05759ec4bb06126cac62697c9e2c075af82b6
SunJung <djtjswjd1@naver.com>
2025-02-18 09:42:08 +0900

    [CHORE] 기기 예약 수정 일부 보완
    Former-commit-id: 3728b9c6c0b8f9d2c8a243b13f2dddf314ec40da

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/controller/CustomScheduleController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/schedule/controller/CustomScheduleController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d908f180c1e1223e86eb17431d93e1fd16bcb802494299b759ae9ff5c9204adf
-size 12347
+oid sha256:b2bea5791d0e9fa8312e79ef20b9ee43fe3f966ca0383f0c8bdcf7611f6678be
+size 12367

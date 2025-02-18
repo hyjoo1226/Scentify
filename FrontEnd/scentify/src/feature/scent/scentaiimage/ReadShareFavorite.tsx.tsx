06139e7ece9fe87d789ea648e5c44f62e84abeaf
이가희 <lgh7956@naver.com>
2025-02-18 18:25:54 +0900

    💄 Style: [Scent] AI카드 재수정 <이가희>
    Former-commit-id: 9ad50e366e94ad2d94f8e17ab058c28ffd86c512

--- FrontEnd/scentify/src/feature/scent/scentaiimage/ReadShareFavorite.tsx.tsx
+++ FrontEnd/scentify/src/feature/scent/scentaiimage/ReadShareFavorite.tsx.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec0b2fc2617c6c9a112ff5df3e5d3aff0b2ae695de508ede2d4307f482310adc
+oid sha256:6c0c9708a36ba9622ced2e9a86dbe912704375447a13f9f26f9591fc0e646d15
 size 6834

d304ba9ac73d2bd234de4e53303a8e5751e58b1b
이가희 <lgh7956@naver.com>
2025-02-18 16:41:37 +0900

    💄 style: [Scent] AI이미지 생성 중 버튼 비활성화 UI추가 <이가희>
    Former-commit-id: 629ae5b745639fa398d98fe4c6e3bc31b9ef1afc

--- FrontEnd/scentify/src/feature/scent/scentaiimage/ShareFavorite.tsx
+++ FrontEnd/scentify/src/feature/scent/scentaiimage/ShareFavorite.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c74eb0c80834b293c85e1a646e9d4b8ed26b6b7a47d12639a2d4dd77eb786f9b
-size 7765
+oid sha256:c0acfd04f7b7d81e0ed1ae261bc03dcae676ae8ee0d34fdf5d1dc7b6b926d98d
+size 7711

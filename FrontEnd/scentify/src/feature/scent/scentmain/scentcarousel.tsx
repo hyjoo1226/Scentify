51eb84b6faf79c68d001c5951f2a748f585d0992
leegayoung <leegy0720@naver.com>
2025-02-20 08:43:45 +0900

    💄 style: poppins 폰트 적용 및 시작페이지 변경 <이가영>
    Former-commit-id: 14d867109f91aa4b5cc5a0e931b9ea870cd26fd0

--- FrontEnd/scentify/src/feature/scent/scentmain/scentcarousel.tsx
+++ FrontEnd/scentify/src/feature/scent/scentmain/scentcarousel.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ba9793e9e92fbdc473774f1f13b97921e2e3093950c35f349a879a625dade1c
+oid sha256:69d27f0ba4e9b88c34a3533a38478063ce50ca85626e1a1b1cc873c99cf9df41
 size 1777

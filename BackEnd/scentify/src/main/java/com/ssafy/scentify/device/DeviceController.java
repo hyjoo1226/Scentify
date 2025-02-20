0c02076276e1a450d2933503e4fed6f01c2e0df2
SunJung <djtjswjd1@naver.com>
2025-02-20 13:47:56 +0900

    [HOTFIX] 테스트 종료 및 최종 배포
    Former-commit-id: e24b70e44d8af46dd2089341a2b479df556cb9df

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/device/DeviceController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b960954349174e4c99f732fdb837dc271c8e36d4f0faced99b8be90f3ca1227e
-size 23859
+oid sha256:28daa1d60b763794b436fff8a876c0d9c96ea282d7fc52a1e874922c22d5d215
+size 23833
